7661648963b62d21ab98f7695c4c6e01d2f902fa
wenrouzei <wenrouzei@163.com>
2019-08-08 22:34:50 +0800

    feat(queue): app增加监听队列事件执行前后失败事件记录

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,11 @@
 
 namespace App\Providers;
 
+use Illuminate\Queue\Events\JobFailed;
+use Illuminate\Queue\Events\JobProcessed;
+use Illuminate\Queue\Events\JobProcessing;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Queue;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +28,23 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Queue::before(function (JobProcessing $event) {
+            // $event->connectionName
+            // $event->job
+            // $event->job->payload()
+        });
+
+        Queue::after(function (JobProcessed $event) {
+            // $event->connectionName
+            // $event->job
+            // $event->job->payload()
+        });
+
+        Queue::failing(function (JobFailed $event) {
+            Log::error('JobFailed', ['exception' => $event->exception]);
+            // $event->connectionName
+            // $event->job
+            // $event->exception
+        });
     }
 }
