026a08e723e68dcecef057ce71c7a82216110627
wenrouzei <wenrouzei@163.com>
2019-08-13 00:02:06 +0800

    feat(command): 增加启动workerman-gateway命令

---
+++ app/WorkerMan/Event.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wenrouzei
+ * Date: 2019/8/12
+ * Time: 23:13
+ */
+
+namespace App\WorkerMan;
+
+
+use App\User;
+use GatewayWorker\BusinessWorker;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Str;
+
+class Event
+{
+    /**
+     * @param $businessWorker
+     */
+    public static function onWorkerStart(BusinessWorker $businessWorker)
+    {
+        Log::debug('worker-id：' . $businessWorker->workerId);
+    }
+
+    public static function onConnect($clientId)
+    {
+        Log::info('connect：' . $clientId);
+    }
+
+    public static function onWebSocketConnect($clientId, $data)
+    {
+        Log::info('ws connect：' . $clientId, ['$data' => $data]);
+    }
+
+    public static function onMessage($clientId, $message)
+    {
+        $user = new User();
+        $user->name = Str::random(10);
+        $user->email = Str::random(10) . '@' . Str::random(3) . '.com';
+        $user->password = Str::random(16);
+        $user->save();
+        Log::info($clientId . ' message：' . $message);
+    }
+
+    public static function onClose($clientId)
+    {
+        Log::debug('close：' . $clientId);
+    }
+}
